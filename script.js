6b5379d53d6f307fe20ac41da6d0148f4ff223c5
Kimberly Morgan <58289484+Kimberly-Morgan@users.noreply.github.com>
2020-02-10 18:40:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var weather;
+function setup() {
+    createCanvas(200, 200);
+    loadJson('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=21a5a7316c8e8c4061d04f45d5800583')
+}
+
+function gotData(data) {
+    println(data);
+    weather = data;
+}
+
+
+function draw() {
+    background(0);
+    if (weather) {
+        
+    }
+}
