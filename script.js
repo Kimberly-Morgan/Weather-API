febadaa99a11a381cbc5954bdf826b042ed07225
Kimberly Morgan <58289484+Kimberly-Morgan@users.noreply.github.com>
2020-02-27 17:36:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 var weather;
-function setup() {
-    createCanvas(200, 200);
-    loadJson('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=21a5a7316c8e8c4061d04f45d5800583')
-}
+ function setup() {
+     createCanvas(200, 200);
+     loadJson('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=21a5a7316c8e8c4061d04f45d5800583')
+ }
 
-function gotData(data) {
-    println(data);
-    weather = data;
-}
+ function gotData(data) {
+     println(data);
+     weather = data;
+ }
 
 
-function draw() {
-    background(0);
-    if (weather) {
+ function draw() {
+     background(0);
+     if (weather) {
         
-    }
-}
+     }
+ }
+var apiKey = "";
+var city = "Orlando"
+
+$("button").click(function(){
+    $.ajax({
+        type: "GET",
+        BaseURL: `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`})
